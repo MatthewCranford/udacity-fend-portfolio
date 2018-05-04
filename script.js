101bf24d4f8d2a16e7cc67731f0a78d711f4b183
Matthew Cranford <mcranfordmail@gmail.com>
2018-05-04 14:13:23 -0700

    Add menu drawer and move name/role into cover

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-for i in 5
-    ewwe 
+const drawer = document.getElementById('drawer');
+const menu = document.getElementById('menu');
+
+menu.addEventListener('click', () => {
+  if (drawer.classList.contains('open')) {
+    drawer.classList.remove('open');
+  } else {
+    drawer.classList.add('open');
+  }
+});
